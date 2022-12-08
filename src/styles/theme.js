d03bc4ef8db419f9112091141d45bb05f5fcc821
Eunji Jo <0119eunji@gmail.com>
2022-12-08 16:16:31 +0900

    feat : plz rebase

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -7,14 +7,6 @@ const theme = {
   boxColor: '#faf0e6',
   blue: '#536DFE',
   red: '#EB5757',
-  yellow: '#FFAB40',
-  gray100: '#333333',
-  gray200: '#4F4F4F',
-  gray300: '#828282',
-  gray400: '#BDBDBD',
-  gray500: '#E0E0E0',
-  gray600: '#F2F2F2',
-  gray700: '#F9F9F9',
 };
 
 export default theme;
