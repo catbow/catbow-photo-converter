530ff1264ceeec6378a7293bfac613118aa8bef4
joeunji <95282989+Joeunji0119@users.noreply.github.com>
2023-01-13 22:38:50 +0900

    feat : catbow 조건부 url (#49)
    Co-authored-by: Eunji Jo <0119eunji@gmail.com>

--- src/Catbow.js
+++ src/Catbow.js
@@ -3,8 +3,13 @@ import ScrollView from 'react-catbow-scrollview';
 import Home from './pages/Home';
 
 const Catbow = () => {
+  const url =
+    process.env.NODE_ENV === 'development'
+      ? './catbow-photo-converter/image/IMG_'
+      : 'image/IMG_';
+
   const option = {
-    imgUrl: 'image/IMG_',
+    imgUrl: url,
     videoImageCount: 702, // Total-number-of-images
     startNum: 1, // Image-path-start-number
     extension: '.jpg', // Available-with-any-image-extension
