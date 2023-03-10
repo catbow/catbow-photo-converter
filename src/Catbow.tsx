3c5bc7a22ad843129f037cd607619d6de22f3952
kimjuno97 <jh241093@gmail.com>
2023-03-10 20:44:05 +0900

    feat : requestAnimationFrame (#51)
    * feat : requestAnimationFrame
    
    * fix: count오류 수정

--- src/Catbow.tsx
+++ src/Catbow.tsx
@@ -1,6 +1,10 @@
+import { useEffect } from 'react';
 import ScrollView from 'react-catbow-scrollview';
 import Home from './pages/Home';
+import axios from 'axios';
 
+const imageCount = 702;
+// 1~ 703
 const Catbow = () => {
   const url =
     process.env.NODE_ENV === 'development'
@@ -18,6 +22,22 @@ const Catbow = () => {
       height: '100vh',
     },
   };
+  useEffect(() => {
+    function fetchImages(index: number) {
+      if (index > 703) {
+        return;
+      }
+      axios(
+        `https://catbow.github.io/catbow-photo-converter/image/IMG_${index}.jpg`
+      );
+      requestAnimationFrame(() => {
+        fetchImages(index + 1);
+      });
+    }
+    if (process.env.NODE_ENV !== 'development') {
+      fetchImages(1);
+    }
+  }, []);
 
   return <ScrollView option={option} viewItem={<Home />} />;
 };
