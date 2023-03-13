1ba9448341560dc27bf3b91f3bf3b2daec0f15ee
kimjuno97 <jh241093@gmail.com>
2023-03-13 21:53:46 +0900

    feat: image get 최적화

--- src/Catbow.tsx
+++ src/Catbow.tsx
@@ -1,20 +1,47 @@
-import { useEffect } from 'react';
-import ScrollView from 'react-catbow-scrollview';
+import { useEffect, useRef, useState } from 'react';
+import ScrollView, { TypeScrollView } from 'react-catbow-scrollview';
 import Home from './pages/Home';
 import axios from 'axios';
 
+/** 이미지 몇장씩 불러올지 설정 */
+const imageRanges = [
+  { start: 1, end: 70 },
+  { start: 71, end: 140 },
+  { start: 141, end: 210 },
+  { start: 211, end: 280 },
+  { start: 281, end: 350 },
+  { start: 351, end: 420 },
+  { start: 421, end: 490 },
+  { start: 491, end: 560 },
+  { start: 561, end: 630 },
+  { start: 631, end: 703 },
+];
+
 const Catbow = () => {
+  const [ejectRatio, setEjectRatio] = useState(0);
+  const imgGetController = useRef({
+    1: false,
+    2: false,
+    3: false,
+    4: false,
+    5: false,
+    6: false,
+    7: false,
+    8: false,
+    9: false,
+    10: false,
+  });
   const url =
     process.env.NODE_ENV === 'development'
       ? './catbow-photo-converter/image/IMG_'
       : 'image/IMG_';
 
-  const option = {
+  const option: TypeScrollView = {
     imgUrl: url,
-    videoImageCount: 29, // Total-number-of-images
+    videoImageCount: 702, // Total-number-of-images
     startNum: 1, // Image-path-start-number
     extension: '.jpg', // Available-with-any-image-extension
-    scrollAreaY: 1000, // scrollArea,
+    scrollAreaY: 50000, // scrollArea,
     viewPort: {
       width: '100vw',
       height: '100vh',
@@ -22,23 +49,35 @@ const Catbow = () => {
   };
 
   useEffect(() => {
-    function fetchImages(index: number) {
-      if (index > 30) {
+    function fetchImages({ start, end }: { start: number; end: number }) {
+      if (start > end) {
         return;
       }
       axios(
-        `https://catbow.github.io/catbow-photo-converter/image/IMG_${index}.jpg`
+        `https://catbow.github.io/catbow-photo-converter/image/IMG_${start}.jpg`
       );
       requestAnimationFrame(() => {
-        fetchImages(index + 1);
+        fetchImages({ start: start + 1, end });
       });
     }
     if (process.env.NODE_ENV !== 'development') {
-      fetchImages(1);
+      for (let i = 1; i <= imageRanges.length; i++) {
+        const imageRange = imageRanges[i - 1];
+        if (ejectRatio > i * 7 && !imgGetController.current[i]) {
+          fetchImages(imageRange);
+          imgGetController.current[i] = true;
+        }
+      }
     }
-  }, []);
+  }, [ejectRatio]);
 
-  return <ScrollView option={option} viewItem={<Home />} />;
+  return (
+    <ScrollView
+      option={option}
+      setEjectRatio={setEjectRatio}
+      viewItem={<Home />}
+    />
+  );
 };
 
 export default Catbow;
